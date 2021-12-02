0bfe96820a0aadcd8957a16d36f5a49a45a8fab3
David Lam <dlamh19@gmail.com>
2021-12-01 22:13:19 -0500

    xd

--- app/src/main/java/project/fiverupizzeria/MainActivity.java
+++ app/src/main/java/project/fiverupizzeria/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.order_layout);
+        setContentView(R.layout.current_order_layout);
     }
 }
