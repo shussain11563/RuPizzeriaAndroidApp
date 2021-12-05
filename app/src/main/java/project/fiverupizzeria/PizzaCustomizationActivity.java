28dd9a6edfc17c07459bb9a52464e1e6404ec912
David Lam <dlamh19@gmail.com>
2021-12-05 01:34:52 -0500

    added alerts to all activities

--- app/src/main/java/project/fiverupizzeria/PizzaCustomizationActivity.java
+++ app/src/main/java/project/fiverupizzeria/PizzaCustomizationActivity.java
@@ -1,5 +1,6 @@
 package project.fiverupizzeria;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.media.Image;
@@ -8,6 +9,8 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.recyclerview.widget.RecyclerView;
@@ -142,16 +145,83 @@ public class PizzaCustomizationActivity extends AppCompatActivity
 
 
     /**
-     * WE WANT TO DO ONDESTROY AFTER THIS ACTIVITY ENDS
+     * Method that alerts users that their pizza is added to their order.
      */
-
-
     public void addToOrderAlertBox() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Added to Order");
+        alert.setMessage("Order has been added.");
 
+        alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        AlertDialog dialog = alert.create();
+        dialog.show();
     }
 
+    /**
+     * Shows alert box regarding removing an essential topping.
+     */
+    public void showConfirmationRemoveEssentialToppings() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Warning with Removing Toppings");
+        alert.setMessage("You are removing essential toppings");
 
+        alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                callRemoveToppings();
+            }
+        });
 
+        alert.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        AlertDialog dialog = alert.create();
+        dialog.show();
+    }
+
+    /**
+     * Shows alert box when there is no toppings on pizza.
+     */
+    public void showConfirmationNoToppingsOnPizza() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Warning with Removing Toppings");
+        alert.setMessage("No Toppings on Pizza");
+
+        alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        AlertDialog dialog = alert.create();
+        dialog.show();
+    }
+
+    /**
+     * Shows alert box when more than 7 toppings is trying to be added to the pizza
+     */
+    public void showMaxToppingsOnPizza() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Warning with Adding Toppings");
+        alert.setMessage("You can only have at most 7 toppings");
+
+        alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        AlertDialog dialog = alert.create();
+        dialog.show();
+    }
 
     /**
      * Checks whether a topping is essential for a Deluxe Pizza.
